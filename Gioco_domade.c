07667fa85ac203bfd0dd03bf476029c953200ca1
paolo di maio <paolodm925@gmail.com>
2024-03-08 19:04:58 +0100

    modifiche

--- Gioco_domade.c
+++ Gioco_domade.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void menu()
 {
@@ -7,8 +8,8 @@ void menu()
 
 void partita()
 {
-    int score = 0;
-    char nome;
+    int score =0;
+    char nome[10] = {'\0'};
     int risposta1, risposta2, risposta3;
     printf("Inserisci il tuo nome :\n");
     scanf("%s", &nome);
@@ -23,17 +24,52 @@ void partita()
     }
     else
         printf("Risposta errata !\n");
+    
+    printf("La seconda domanda del gioco è : come si stampa a video una stringa in C\n");
+    printf("Scegliere tra le seguenti risposte: \n 1] Scanf\n 2]Printf \n 3] If\n");
+    scanf("%d", &risposta2);
+
+    if (risposta2 == 2)
+    {
+        printf("Risposta esatta !\n");
+        score++;
+    }
+    else
+        printf("Risposta errata !\n");
+
+    printf("La terza domanda del gioco è : Qual è il carattere per inserire un numero intero C\n");
+    printf("Scegliere tra le seguenti risposte: \n 1] S\n 2] D\n 3] C\n");
+    scanf("%d", &risposta3);
+
+    if (risposta3 == 2)
+    {
+        printf("Risposta esatta !\n");
+        score++;
+    }
+    else
+        printf("Risposta errata !\n");
+
+    printf("Le domande sono finte %s, hai totalizzato uno score pari a: %d\n", nome ,score);
+        
+    menu();
 }
 int main()
 {
-
+    
     menu();
+    partita();
+    
+    
     char scelta;
     scanf("%s", &scelta);
     while (scelta == 'A')
     {
-        menu();
-        partita();
+        
+        main();
+        
+        
+        
+        
         
     }
 
@@ -42,4 +78,6 @@ int main()
 
         return 0;
     }
+    
+    
 }
